cea3a58a7796562dce4bab6142e2dfb8537cafd1
Antony <antonyni1010@gmail.com>
2023-08-31 09:50:55 -0400

    set up keycloak

---
+++ demokeycloak/src/components/Public.jsx
@@ -0,0 +1,9 @@
+const Public = () => {
+    return(
+        <>
+        <div>Public</div>
+        </>
+    )
+}
+
+export default Public;
---
+++ demokeycloak/src/hooks/useAuth.jsx
@@ -0,0 +1,21 @@
+import { useState,useEffect } from "react";
+import Keycloak from "keycloak-js";
+const useAuth = () => {
+    const [isLogin, setLogin] = useState(false);
+    useEffect(()=>{
+        const client = new Keycloak({
+            url: import.meta.env.VITE_KEYCLOAK_URL,
+            realm: import.meta.env.VITE_KEYCLOAK_REALM,
+            clientId: import.meta.env.VITE_KEYCLOAK_CLIENT
+        });
+        client.init({
+            onLoad: "login-required"
+        }).then((res)=>setLogin(res))
+    },[]
+    )
+    return(
+        isLogin
+    );
+}
+
+export default useAuth;
